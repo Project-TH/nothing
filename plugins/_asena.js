8c2c363e4a205a749803607b7165d869bd873419
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-11 14:48:44 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -15,7 +15,7 @@ const Lang = Language.getString('_asena');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'ahelp ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: '8989 ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
     var CMD_HELP = '';
     if (match[1] === '') {
         Asena.commands.map(
@@ -83,7 +83,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'ahelp ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: '8989 ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
     var CMD_HELP = '';
     if (match[1] === '') {
         Asena.commands.map(
@@ -147,7 +147,7 @@ else if (Config.WORKTYPE == 'public') {
     }
 }));
 
-Asena.addCommand({pattern: 'ahelp ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
+Asena.addCommand({pattern: '8989 ?(.*)', fromMe: true, dontAddCMDList: true}, (async (message, match) => {
     var CMD_HELP = '';
     if (match[1] === '') {
         Asena.commands.map(
