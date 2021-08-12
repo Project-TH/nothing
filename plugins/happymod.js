9ee594f56bfa4dd5ae8c55638aac67b904df156a
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-12 09:09:47 +0530

    Create happymod.js

---
+++ plugins/happymod.js
@@ -0,0 +1,60 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+const SIZ = "Size"
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+
+if (Config.WORKTYPE == 'private') {
+	
+Asena.addCommand({pattern: 'hpmod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=eDDV8wqAaTTmekir6SE5og9FB6O`;
+	try {
+		const response = await got(url);
+		const hpmod = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*📕 ' + Lang.NAMEY +'* ```' + hpmod.result[0].title + '```\n' + 
+		'*📘 ' + 'Size' +'* ```' + hpmod.result[0].size + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + hpmod.result[0].link + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});
+}
+else if (Config.WORKTYPE == 'public') {
+	
+Asena.addCommand({pattern: 'hpmod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=eDDV8wqAaTTmekir6SE5og9FB6O`;
+	try {
+		const response = await got(url);
+		const hpmod = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*📕 ' + Lang.NAMEY +'* ```' + hpmod.result[0].title + '```\n' + 
+		'*📘 ' + 'Size' +'* ```' + hpmod.result[0].size + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + hpmod.result[0].link + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});
+
+Asena.addCommand({pattern: 'hpmod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=eDDV8wqAaTTmekir6SE5og9FB6O`;
+	try {
+		const response = await got(url);
+		const hpmod = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*📕 ' + Lang.NAMEY +'* ```' + hpmod.result[0].title + '```\n' + 
+		'*📘 ' + 'Size' +'* ```' + hpmod.result[0].size + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + hpmod.result[0].link + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});
+
+
+}
