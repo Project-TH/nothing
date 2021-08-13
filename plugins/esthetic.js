65dd44ba326efce1cc26490e9f8c22e8004661dc
~ Thevindu ~ <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-13 12:14:44 +0530

    Update esthetic.js

--- plugins/esthetic.js
+++ plugins/esthetic.js
@@ -7,6 +7,18 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'public') {
 
+    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: false,dontaddCommandList: true }, (async (message, match) => {
+      
+        var ttinullimage = await axios.get(`https://api.${Config.CCRUN}.xyz/api/estetikpic?apikey=${Config.TH}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+    
+    }));
+
+}
+
+else if (Config.WORKTYPE == 'private') {
+
     Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: true,dontaddCommandList: true }, (async (message, match) => {
       
         var ttinullimage = await axios.get(`https://api.${Config.CCRUN}.xyz/api/estetikpic?apikey=${Config.TH}`, { responseType: 'arraybuffer' })
