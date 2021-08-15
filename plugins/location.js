4dd5fcc936046ec4c9f75522448cc9bf09060498
~ Thevindu ~ <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-15 10:20:00 +0530

    Update location.js

--- plugins/location.js
+++ plugins/location.js
@@ -1,8 +1,3 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
-*/
-
 const Asena = require('../events');
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys'); // Boredom 😬
 
@@ -13,11 +8,11 @@ const Lang = Language.getString('locate'); // Language supp. 😉
     Asena.addCommand({pattern: 'locate', fromMe: true, desc: Lang.L_DESC, warn: Lang.L_WARN}, (async (message, match) => {
 
         var r_text = new Array ();
-        r_text[0] = "degreesLatitude: 24.121231, degreesLongitude: 55.1121221"; // Actually, I don't know where is this place..
-        r_text[1] = "degreesLatitude: 8.838637, degreesLongitude: -13.721434"; // U too homie
+        r_text[0] = "𝙳𝙴𝙶𝚁𝙴𝙴𝚂 𝙻𝙰𝚃𝙸𝚃𝚄𝙳𝙴: 24.121231, 𝙳𝙴𝙶𝚁𝙴𝙴𝚂 𝙻𝙾𝙽𝙶𝙸𝚃𝚄𝙳𝙴: 55.1121221"; // Actually, I don't know where is this place..
+        r_text[1] = "𝙳𝙴𝙶𝚁𝙴𝙴𝚂 𝙻𝙰𝚃𝙸𝚃𝚄𝙳𝙴: 8.838637, 𝙳𝙴𝙶𝚁𝙴𝙴𝚂 𝙻𝙾𝙽𝙶𝙸𝚃𝚄𝙳𝙴: -13.721434"; // U too homie
 
         var i = Math.floor(2*Math.random()) // Random func. 🤪
 
-        await message.sendMessage(`My Location! ${r_text[i]}`, MessageType.location); // It sends ur location. Cool tho 😱
+        await message.sendMessage(`𝙼𝚈 𝙻𝙾𝙲𝙰𝚃𝙸𝙾𝙽! ${r_text[i]}`, MessageType.location); // It sends ur location. Cool tho 😱
 
 }));
