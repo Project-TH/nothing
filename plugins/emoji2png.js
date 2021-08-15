d9bb6d45ebf7c48993ec5393905bcc526d753e82
~ Thevindu ~ <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-15 10:11:06 +0530

    Create emoji2png.js

---
+++ plugins/emoji2png.js
@@ -0,0 +1,45 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const request = require('request');
+const got = require("got");
+const Config = require('../config');
+const des = "EMOJI TO PNG"
+const iii = "```YOU MUST ENTER EMOJI!```"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'png ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.CAPTION_KEY})
+
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'png ?(.*)', fromMe: false, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.CAPTION_KEY})
+
+    }));
+
+
+    Asena.addCommand({pattern: 'png ?(.*)', fromMe: true, desc: des}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(iii);
+
+        var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/emoji2png?emoji=${encodeURIComponent(match[1])}&type=apple`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.CAPTION_KEY})
+
+    }));
+}
